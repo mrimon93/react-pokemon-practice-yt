427d8a88cd76ebbf0c5674485d7b9a0423d5dfcb
mrimon93 <muzammil_kamaly93@live.se>
2023-12-27 09:25:12 +0100

    issue with axios package

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [data, setData] = useState()
-  const [name, setName] = useState()
+  const [data, setData] = useState();
+  const [name, setName] = useState();
 
   const URL = "https://pokeapi.co/api/v2/pokemon/ditto"
 
@@ -13,15 +13,16 @@ function App() {
     axios.get(URL).then((response) => {
       //console.log(response.data
       setData(response.data);
+      setName(response.data.name);
     }).catch(err) => {
       window.alert(err);
     }
-
   })
 
   return (
     <div className="App">
     <h1>Pokemon</h1>
+    <h2>{name}</h2>
     </div>
   );
 }
